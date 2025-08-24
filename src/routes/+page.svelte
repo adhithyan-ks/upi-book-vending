48d5961a01965001fe40c3888b59872676517966
adhithyan-ks <adhithyanktd@gmail.com>
2025-08-24 15:08:42 +0000

    add NewCounter component and integrate it into the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,10 @@
 <script>
 	import Counter from "$lib/components/Counter.svelte";
-    import Button from "$lib/components/Button.svelte";
+	import NewCounter from "$lib/components/NewCounter.svelte";
 </script>
 <main>
     
-    <Counter />
-    <br />
-    <br />
-    <Button onclick={() => alert('Button clicked!')}>Buy books</Button>
+    <NewCounter />
+    <!-- <Button onclick={() => alert('Button clicked!')}>Buy books</Button> -->
 </main>
-
-<style>
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    }
-</style>
-
   
